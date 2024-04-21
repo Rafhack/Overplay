5c948ad579326bbb7cf0330c9fc60983707ac688
Rafhack <rafael.flip@hotmail.com>
2024-04-21 15:22:41 -0300

    Added Axis data to ViewState

--- app/src/main/java/com/example/overplay/MainActivity.kt
+++ app/src/main/java/com/example/overplay/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.example.overplay.MainViewState.TiltSensorData.TiltSensorState
 import com.example.overplay.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
@@ -18,21 +19,30 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
         viewModel.dispatch(MainUserAction.ViewScreen)
-
         lifecycleScope.launch {
             viewModel.mainStateFlow.collect(::renderViewState)
         }
     }
 
     private fun renderViewState(viewState: MainViewState) {
-        Toast.makeText(this, if (viewState.isLoading) {
-            "Loading..."
-        } else {
-            "Ready!"
-        }, Toast.LENGTH_SHORT).show()
+        handleTiltState(viewState.tiltSensorData.tiltState)
+        Toast.makeText(
+            this, if (viewState.playerState.isLoading) {
+                "Loading..."
+            } else {
+                "Ready!"
+            }, Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun handleTiltState(state: TiltSensorState) = when (state) {
+        TiltSensorState.TILTING_LEFT -> {}
+        TiltSensorState.TILTING_RIGHT -> TODO()
+        TiltSensorState.TILTING_UP -> TODO()
+        TiltSensorState.TILTING_DOWN -> TODO()
+        TiltSensorState.IDLE -> TODO()
     }
 }
--- app/src/main/java/com/example/overplay/MainContract.kt
+++ app/src/main/java/com/example/overplay/MainContract.kt
@@ -1,11 +1,46 @@
 package com.example.overplay
 
+import com.example.overplay.MainViewState.TiltSensorData.TiltSensorState
+
 data class MainViewState(
-    val isLoading: Boolean
+    val playerState: PlayerState,
+    val tiltSensorData: TiltSensorData
 ) {
+
+    data class PlayerState(
+        val isLoading: Boolean
+    )
+
+    data class TiltSensorData(
+        val xAxisData: AxisData,
+        val zAxisData: AxisData,
+        val tiltState: TiltSensorState
+    ) {
+        enum class TiltSensorState {
+            TILTING_LEFT,
+            TILTING_RIGHT,
+            TILTING_UP,
+            TILTING_DOWN,
+            IDLE
+        }
+
+        data class AxisData(
+            val current: Double = 0.0,
+            val origin: Double = 0.0,
+            val offset: Double = 0.0
+        )
+    }
+
     companion object {
         val INITIAL_STATE = MainViewState(
-            isLoading = true
+            playerState = PlayerState(
+                isLoading = true
+            ),
+            tiltSensorData = TiltSensorData(
+                xAxisData = TiltSensorData.AxisData(),
+                zAxisData = TiltSensorData.AxisData(),
+                tiltState = TiltSensorState.IDLE
+            )
         )
     }
 }
