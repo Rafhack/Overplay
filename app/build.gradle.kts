279fc3ad719cdcc3d718d904b0696601241630bf
Rafhack <rafael.flip@hotmail.com>
2024-04-21 13:38:52 -0300

    Removed default project files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,16 +34,23 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    kapt {
+        correctErrorTypes = true
+    }
 }
 
 dependencies {
-
+    implementation(libs.kotlinx.coroutines)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/overplay/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.overplay
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MainViewModel : ViewModel() {
+
+    private val _mainStateFlow = MutableStateFlow()
+
+}
